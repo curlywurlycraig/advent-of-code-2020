ed81703c438f39b65972a762dd3bc636d0ba6ea3
Craig Wilkinson <craig.wilkinson@unity3d.com>
2020-12-08 09:38:06 +0000

    Add part two result to day 1

--- day-1-sum/src/main.rs
+++ day-1-sum/src/main.rs
@@ -4,7 +4,7 @@ mod input;
 use input::input;
 
 fn main() {
-    let result = input
+    let part_one_result = input
         .iter()
         .tuple_combinations()
         .filter(|(a, b)| *a + *b == 2020)
@@ -12,5 +12,15 @@ fn main() {
         .next()
         .unwrap();
 
-    println!("Result: {}", &result);
+    println!("Result: {}", &part_one_result);
+
+    let part_two_result = input
+        .iter()
+        .tuple_combinations()
+        .filter(|(a, b, c)| *a + *b + *c == 2020)
+        .map(|(a, b, c)| a * b * c)
+        .next()
+        .unwrap();
+
+    println!("Part two result: {}", &part_two_result);
 }
